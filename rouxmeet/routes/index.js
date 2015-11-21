024ace3c31bf72fd7445c028bdb32bfa25f45dcd
murataka2015 <murat-aka@hotmail.com>
2015-11-21 01:54:08 +0000

    update index file and make data.json global variable

--- rouxmeet/routes/index.js
+++ rouxmeet/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var appdata = require('../data.json');
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {
+
+  res.render('index', { 
+  	title: 'Home',
+  	page: 'home'
+  });
 });
 
 module.exports = router;
